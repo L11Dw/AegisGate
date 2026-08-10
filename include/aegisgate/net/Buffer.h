c7ea6dbe554ccfc4cc720414bfa182466be93676
bubu <bubulan.code@gmail.com>
2026-08-10 13:33:19 +0800

    feat: add consumable network buffer

--- include/aegisgate/net/Buffer.h
+++ include/aegisgate/net/Buffer.h
@@ -1,7 +1,23 @@
 #pragma once
 
+#include <cstddef>
+#include <string>
+#include <string_view>
+
 namespace aegisgate::net {
 
-class Buffer {};
+class Buffer {
+public:
+  void Append(std::string_view bytes);
+  [[nodiscard]] std::size_t ReadableBytes() const noexcept;
+  [[nodiscard]] std::string_view ReadableView() const noexcept;
+  [[nodiscard]] std::size_t FindCrlf() const noexcept;
+  void Retrieve(std::size_t length);
+  void RetrieveAll() noexcept;
 
-}  // namespace aegisgate::net
+private:
+  std::string storage_;
+  std::size_t read_index_ = 0;
+};
+
+} // namespace aegisgate::net
--- src/net/Buffer.cpp
+++ src/net/Buffer.cpp
@@ -1,1 +1,45 @@
 #include "aegisgate/net/Buffer.h"
+
+#include <stdexcept>
+
+namespace aegisgate::net {
+
+void Buffer::Append(std::string_view bytes) {
+  if (bytes.empty()) {
+    return;
+  }
+
+  if (read_index_ > ReadableBytes()) {
+    storage_.erase(0, read_index_);
+    read_index_ = 0;
+  }
+
+  storage_.append(bytes.data(), bytes.size());
+}
+
+std::size_t Buffer::ReadableBytes() const noexcept {
+  return storage_.size() - read_index_;
+}
+
+std::string_view Buffer::ReadableView() const noexcept {
+  return std::string_view(storage_).substr(read_index_);
+}
+
+std::size_t Buffer::FindCrlf() const noexcept {
+  return ReadableView().find("\r\n");
+}
+
+void Buffer::Retrieve(std::size_t length) {
+  if (length > ReadableBytes()) {
+    throw std::out_of_range("retrieve length exceeds readable bytes");
+  }
+
+  read_index_ += length;
+}
+
+void Buffer::RetrieveAll() noexcept {
+  storage_.clear();
+  read_index_ = 0;
+}
+
+} // namespace aegisgate::net
